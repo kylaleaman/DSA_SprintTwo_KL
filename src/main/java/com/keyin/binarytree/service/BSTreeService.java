ebe4518a2e590552cd133c1f292550536534e441
kylaleaman <138683217+kylaleaman@users.noreply.github.com>
2024-08-11 21:01:21 -0230

    Adding service, repository and controller files as well as create folders for testing

--- src/main/java/com/keyin/binarytree/service/BSTreeService.java
+++ src/main/java/com/keyin/binarytree/service/BSTreeService.java
@@ -1,4 +1,14 @@
 package com.keyin.binarytree.service;
 
-public class BSTreeService {
+import com.keyin.binarytree.model.BSTree;
+import com.keyin.binarytree.model.BSTreeEntity;
+
+import java.util.List;
+
+public interface BSTreeService {
+    BSTree createTree(List<Integer> numbers);
+    String convertTreeToJson(BSTree tree);
+    BSTreeEntity saveTreeEntity(BSTreeEntity treeEntity);
+    List<BSTreeEntity> getAllTrees();
+    BSTreeEntity getTreeById(Long id);
 }
