7552649bd130e6e25cfd4694a2596e68cd198efe
kylaleaman <138683217+kylaleaman@users.noreply.github.com>
2024-08-11 19:23:04 -0230

    Adding files into model folder: BSTree, BSTreeEntity, BSTreeNode

--- src/main/java/com/keyin/binarytree/controller/BSTreeController.java
+++ src/main/java/com/keyin/binarytree/controller/BSTreeController.java
@@ -1,6 +1,7 @@
 package com.keyin.binarytree.controller;
 
 import com.keyin.binarytree.model.BSTree;
+import com.keyin.binarytree.service.BSTreeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,11 +11,11 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
-public class TreeController {
+public class BSTreeController {
 
     // Assuming you have a service to handle database operations
     @Autowired
-    private TreeService treeService;
+    private BSTreeService bsTreeService;
 
     @PostMapping("/process-numbers")
     public ResponseEntity<Map<String, Object>> processNumbers(@RequestBody List<Integer> numbers) {
@@ -23,9 +24,7 @@ public class TreeController {
 
         List<Integer> treeList = bst.toList();
 
-        // Save numbers and tree structure to the database
-        treeService.saveNumbersAndTree(numbers, treeList);
-
+        // Save numbers and tree structure to the database\
         return ResponseEntity.ok(Map.of("tree", treeList));
     }
 }
