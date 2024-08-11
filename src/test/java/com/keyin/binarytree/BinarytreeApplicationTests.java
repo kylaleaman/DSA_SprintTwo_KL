2852fca8395f2b005c4667ba48987f882d65ece8
kylaleaman <138683217+kylaleaman@users.noreply.github.com>
2024-08-11 17:22:43 -0230

    Initial commit
    q

---
+++ src/test/java/com/keyin/binarytree/BinarytreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keyin.binarytree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinarytreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
